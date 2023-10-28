54bf042fe036c264ed1cdc8f9060a7559103992e
SijadWalipoor <Sijad.walipoor@student.hogent.be>
2023-10-28 21:38:26 +0200

    vierde commit

--- arrow.js
+++ arrow.js
@@ -1,20 +1,18 @@
-window.addEventListener("scroll", function() {
-    window.onscroll = function() {
-        // Get the current scroll position
-        var scrollTop = window.scrollY || document.documentElement.scrollTop;
-    
-        // Get the total height of the document, including the scrollable area
-        var totalHeight = document.documentElement.scrollHeight;
-    
-        // Get the height of the viewport (visible area)
-        var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-    
-        // Calculate how close the user is to the end of the page
-        var scrollPercentage = (scrollTop + viewportHeight) / totalHeight * 100;
-    
-        // Check if the user is near the end (e.g., within the last 10% of the page)
-        if (scrollPercentage >= 90) {
-            alert("You're at or near the end of the page!");
-        }
-    };
+window.addEventListener("scroll", function () {
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+    var totalHeight = document.documentElement.scrollHeight;
+    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+    var scrollPercentage = (scrollTop + viewportHeight) / totalHeight * 100;
+
+    if (scrollPercentage >= 99) {
+        var arrowIcon = document.querySelector(".fa-solid.fa-arrow-up.fa-2xl");
+
+        // Add the "fa-bounce" class to the arrow icon
+        arrowIcon.classList.add("fa-bounce");
+
+        // Start the bounce animation for 3 seconds
+        setTimeout(function () {
+            arrowIcon.classList.remove("fa-bounce"); // Remove the class after the animation
+        }, 5000);
+    }
 });
